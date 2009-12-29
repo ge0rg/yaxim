a8c3c18bc618eb4f17a785b7039bb9c75fbd29d8
Chris Soyars <chris@slightlycooler.net>
2009-12-29 17:02:52 -0500

    Added DBAdapter class.

---
+++ src/org/yaxim/androidclient/data/DBAdapter.java
@@ -0,0 +1,37 @@
+package org.yaxim.androidclient.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+public class DBAdapter extends SQLiteOpenHelper {
+	private static SQLiteDatabase mSQLiteDatabase;
+	private static DBAdapter instance;
+
+	private static final String DATABASE_NAME = "yaxim";
+	private static final int DATABASE_VERSION = 1;
+
+	private DBAdapter(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	public void onCreate(SQLiteDatabase db) {
+
+	}
+
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+		onCreate(db);
+	}
+
+	public void initialize(Context context) {
+		if (instance == null) {
+			instance = new DBAdapter(context, DATABASE_NAME, null, DATABASE_VERSION);
+			mSQLiteDatabase = instance.getWritableDatabase();
+		}
+	}
+
+	public SQLiteDatabase getDatabase() {
+		return mSQLiteDatabase;
+	}
+}
