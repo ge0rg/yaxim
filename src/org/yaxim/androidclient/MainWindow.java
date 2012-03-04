a80b1fa7d5280feb352bab0d191072f6d9a2351c
tuxmobil <tuxmobil@gmx.de>
2012-03-04 23:17:55 +0100

    Remove "cancel" from roster context menu
    The common design pattern for android apps is to leave a context menu
    by pressing the back button.

--- src/org/yaxim/androidclient/MainWindow.java
+++ src/org/yaxim/androidclient/MainWindow.java
@@ -441,10 +441,6 @@ public class MainWindow extends ExpandableListActivity {
 			case R.id.roster_contextmenu_contact_change_group:
 				moveRosterItemToGroupDialog(userJid);
 				return true;
-
-			case R.id.roster_exit:
-				closeContextMenu();
-				return true;
 			}
 		} else {
 
@@ -458,9 +454,6 @@ public class MainWindow extends ExpandableListActivity {
 				renameRosterGroupDialog(seletedGroup);
 				return true;
 
-			case R.id.roster_exit:
-				closeContextMenu();
-				return true;
 			}
 		}
 		return false;
