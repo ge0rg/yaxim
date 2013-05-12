197393189b8116a854d1805fcfd1b26945e466a0
Dario Ernst <daddel9@nebuk.de>
2013-05-12 18:28:57 +0200

    MUCs dont get deleted upon reconnect/roster-cleanup anymore

--- src/org/yaxim/androidclient/service/SmackableImp.java
+++ src/org/yaxim/androidclient/service/SmackableImp.java
@@ -403,6 +403,7 @@ public class SmackableImp implements Smackable {
 		Collection<RosterEntry> rosterEntries = mRoster.getEntries();
 		StringBuilder exclusion = new StringBuilder(RosterConstants.JID + " NOT IN (");
 		boolean first = true;
+		
 		for (RosterEntry rosterEntry : rosterEntries) {
 			updateRosterEntryInDB(rosterEntry);
 			if (first)
@@ -411,7 +412,8 @@ public class SmackableImp implements Smackable {
 				exclusion.append(",");
 			exclusion.append("'").append(rosterEntry.getUser()).append("'");
 		}
-		exclusion.append(")");
+		
+		exclusion.append(") AND GROUP NOT 'MUCs';");
 		int count = mContentResolver.delete(RosterProvider.CONTENT_URI, exclusion.toString(), null);
 		Log.d(TAG, "deleted " + count + " old roster entries");
 	}
