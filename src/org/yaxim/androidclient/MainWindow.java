86eb265b51ffb622d79d8b454d40054ffe31cf74
Dario Ernst <daddel9@nebuk.de>
2013-05-30 11:05:02 +0200

    made the color calculation worse but light-theme-compatible

--- src/org/yaxim/androidclient/MainWindow.java
+++ src/org/yaxim/androidclient/MainWindow.java
@@ -1227,6 +1227,7 @@ public class MainWindow extends SherlockExpandableListActivity {
 				public void onClick(DialogInterface dialog, int which) {
 					// do nothing
 				}});
+			
 			AlertDialog dialog = builder.create();
 			dialog.show();
 		}
