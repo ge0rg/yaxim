640f9228dc6a45726383fd04e8788cb9f3c96aed
Dario Ernst <daddel9@nebuk.de>
2013-06-12 10:17:42 +0200

    fixed beep on own name, added debug msgs

--- src/org/yaxim/androidclient/service/XMPPServiceCallback.java
+++ src/org/yaxim/androidclient/service/XMPPServiceCallback.java
@@ -3,7 +3,7 @@ package org.yaxim.androidclient.service;
 import org.jivesoftware.smack.packet.Message;
 
 public interface XMPPServiceCallback {
-	void newMessage(String from, String messageBody, boolean silent_notification, Message.Type msgType);
+	void newMessage(String[] from, String messageBody, boolean silent_notification, Message.Type msgType);
 	void connectionStateChanged();
 	void rosterChanged(); // TODO: remove that!
 	void mucInvitationReceived(String room, String body);
